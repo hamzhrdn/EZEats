d7e62b1db7084f84c436bc6a969fed626ae18180
hamzhrdn <uhamzahr04@gmail.com>
2024-06-03 14:52:51 +0700

    Initial commit

--- app/src/main/java/com/example/ezeats/MainActivity.kt
+++ app/src/main/java/com/example/ezeats/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.ezeats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        for (i in 0 until bottomNavigationView.menu.size()) {
+            val menuItem = bottomNavigationView.menu.getItem(i)
+            menuItem.title = ""
+        }
+
     }
 }
