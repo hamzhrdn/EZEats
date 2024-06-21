4963daa2b86ff72fb46fc0c00a4542d71bcde21b
hamzhrdn <uhamzahr04@gmail.com>
2024-06-21 14:53:47 +0700

    category

--- app/src/main/java/com/example/ezeats/MainActivity.kt
+++ app/src/main/java/com/example/ezeats/MainActivity.kt
@@ -1,62 +1,51 @@
 package com.example.ezeats
 
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.animation.OvershootInterpolator
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.ezeats.addrecipe.AddRecipeFragment
 import com.example.ezeats.databinding.ActivityMainBinding
-import com.example.ezeats.home.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
-    private lateinit var auth: FirebaseAuth
     private lateinit var navController: NavController
     private lateinit var bottomNav : BottomNavigationView
-    private lateinit var homeFragment: HomeFragment
-    private lateinit var addRecipeFragment: AddRecipeFragment
     @SuppressLint("CutPasteId")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding!!.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
-        navController = navHostFragment.navController
+        val navController = navHostFragment.navController
 
         bottomNav = findViewById(R.id.bottomNavigationView)
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.home -> {
                     Log.d("Item", "home")
+                    Log.d("MainActivity", "NavController: $navController")
                     navController.navigate(R.id.homeFragment)
 //                    loadFragment(HomeFragment())
                     true
                 }
                 R.id.search -> {
                     Log.d("Item", "search")
+                    Log.d("MainActivity", "NavController: $navController")
                     navController.navigate(R.id.searchFragment)
 //                    loadFragment(SearchFragment())
                     true
                 }
                 R.id.add -> {
                     Log.d("Item", "add recipe")
+                    Log.d("MainActivity", "NavController: $navController")
                     navController.navigate(R.id.addRecipeFragment)
 //                    loadFragment(AddRecipeFragment())
                     true
@@ -67,6 +56,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.account -> {
                     Log.d("Item", "profile")
+                    Log.d("MainActivity", "NavController: $navController")
                     navController.navigate(R.id.profileFragment)
 //                    loadFragment(ProfileFragment())
                     true
