9d07f5b0fd61cf4de71fbbcc2b6a8fe528af5ac9
aurel04 <s160421115@student.ubaya.ac.id>
2024-06-21 10:40:54 +0700

    Search Recipe

--- app/src/main/java/com/example/ezeats/network/ApiService.kt
+++ app/src/main/java/com/example/ezeats/network/ApiService.kt
@@ -2,11 +2,13 @@ package com.example.ezeats.network
 
 import com.example.ezeats.response.DetailRecipeResponse
 import com.example.ezeats.response.HomeResponse
+import com.example.ezeats.response.SearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.QueryMap
 
 interface ApiService {
     @GET("home")
@@ -20,6 +22,12 @@ interface ApiService {
         @Query("page") page: Int,
         @Query("size") size: Int
     ): HomeResponse
+
+    @GET("search-by-ingredients")
+    suspend fun getSearchRecipeList(
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): SearchResponse
 
     @POST("add-recipe")
     suspend fun addRecipe()
